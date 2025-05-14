01caf622ce751800b03f7bb458829c562d0ff21c
Boburjon Azimov <azimovbob@gmail.com>
2025-05-14 15:40:03 -0400

    adding subnet id outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "public_subnets" {
+  value = [for subnet in aws_subnet.aws_subnet.private : subnet.id]
+}
+
+output "private_subnets" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
